c5eddc0e7336b36ef32424cbd0e7657efdc50302
OhLaughing <bigwanggang@163.com>
2019-02-22 08:55:13 +0800

    Create TimeServerHandler.java

---
+++ src/com/phei/netty/chapter4/packageSplicing/TimeServerHandler.java
@@ -0,0 +1,37 @@
+package com.phei.netty.chapter4.packageSplicing;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+
+import java.util.Date;
+
+public class TimeServerHandler extends ChannelHandlerAdapter {
+
+    private int count;
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf buf = (ByteBuf) msg;
+        byte[] req = new byte[buf.readableBytes()];
+        buf.readBytes(req);
+        System.out.println("receive: " + new String(req, "UTF-8"));
+        String body = new String(req, "UTF-8").substring(0, req.length -
+                System.getProperty("line.separator").length());
+        System.out.println("The time server receive order: " + body +
+                " ; counter is : " + ++count);
+        String currentTime = "QUERY TIME ORDER".equalsIgnoreCase(body) ?
+                new Date(System.currentTimeMillis()).toString() : "BAD ORDER";
+        currentTime = currentTime + System.getProperty("line.separator");
+        ByteBuf resp = Unpooled.copiedBuffer(currentTime.getBytes());
+        ctx.writeAndFlush(resp);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+
+}
